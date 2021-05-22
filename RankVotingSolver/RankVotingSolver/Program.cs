3fbf07b48b6bfca913c07cc54ff6172607b8902b
Fausto Ruvalcaba <se.fausto@gmail.com>
2021-05-22 14:18:54 -0700

    Rank solver

---
+++ RankVotingSolver/RankVotingSolver/Program.cs
@@ -0,0 +1,234 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+
+namespace RankVotingSolver
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //GenerateRakings();
+
+            var solution = new List<string>
+             {
+                "Shubham",
+                "Sammie",
+                "Rebecca",
+                "Chris",
+                "Joey",
+                "Miranda",
+                "Mercedeze"
+            };
+
+
+
+            var rating = new Ratings();
+            rating.Rankings.Add(
+                new Ranking
+                {
+                    Id = "Shubham",
+                    Ranks = new List<Element> //shubham
+                    {
+                        new Element{Value= "Joey", IsKnownValue =false},
+                        new Element{Value= "Rebecca", IsKnownValue =false},
+                        new Element{Value= "Sammie", IsKnownValue =true},
+                        new Element{Value= "Mercedeze", IsKnownValue =true},
+                        new Element{Value= "Miranda", IsKnownValue =false},
+                        new Element{Value= "Chris", IsKnownValue =true},
+
+                    }
+                }
+                );
+            rating.Rankings.Add(
+                new Ranking
+                {
+                    Id = "Miranda",
+                    Ranks = new List<Element>
+                    {
+                        new Element{Value= "Joey", IsKnownValue =true},
+                        new Element{Value= "Shubham", IsKnownValue =false},
+                        new Element{Value= "Sammie", IsKnownValue =true},
+                        new Element{Value= "Rebecca", IsKnownValue =false},
+                        new Element{Value= "Chris", IsKnownValue =false},
+                        new Element{Value= "Mercedeze", IsKnownValue =false},
+                    }
+                }
+                );
+            rating.Rankings.Add(
+                new Ranking
+                {
+                    Id = "Joey",
+                    Ranks = new List<Element>
+                    {
+                        new Element{Value= "Shubham", IsKnownValue =false},
+                        new Element{Value= "Chris", IsKnownValue =false},
+                        new Element{Value= "Miranda", IsKnownValue =true},
+                        new Element{Value= "Rebecca", IsKnownValue =false},
+                        new Element{Value= "Mercedeze", IsKnownValue =false},
+                        new Element{Value= "Sammie", IsKnownValue =false},
+                    }
+                }
+                );
+            rating.Rankings.Add(
+                           new Ranking
+                           {
+                               Id = "Chris",
+                               Ranks = new List<Element>
+                               {
+                        new Element{Value= "Mercedeze", IsKnownValue =true},
+                        new Element{Value= "Rebecca", IsKnownValue =false},
+                        new Element{Value= "Joey", IsKnownValue =false},
+                        new Element{Value= "Shubham", IsKnownValue =true},
+                        new Element{Value= "Sammie", IsKnownValue =false},
+                        new Element{Value= "Miranda", IsKnownValue =false},
+                               }
+                           }
+                           );
+
+            rating.Rankings.Add(
+                        new Ranking
+                        {
+                            Id = "Mercedeze",
+                            Ranks = new List<Element>
+                            {
+                        new Element{Value= "Chris", IsKnownValue =true},
+                        new Element{Value= "Shubham", IsKnownValue =false},
+                        new Element{Value= "Sammie", IsKnownValue =false},
+                        new Element{Value= "Miranda", IsKnownValue =false},
+                        new Element{Value= "Rebecca", IsKnownValue =true},
+                        new Element{Value= "Joey", IsKnownValue =true},
+                        
+                            }
+                        }
+                        );
+
+            rating.Rankings.Add(
+            new Ranking
+            {
+                Id = "Sammie",
+                Ranks = new List<Element> //Sammie
+                {
+                        new Element{Value= "Chris", IsKnownValue =false},
+                        new Element{Value= "Rebecca", IsKnownValue =true},
+                        new Element{Value= "Joey", IsKnownValue =false},
+                        new Element{Value= "Miranda", IsKnownValue =true},
+                        new Element{Value= "Mercedeze", IsKnownValue =false},
+                        new Element{Value= "Shubham", IsKnownValue =false},
+                }
+            }
+            );
+
+            rating.Rankings.Add(
+           new Ranking
+           {
+               Id = "Rebecca",
+               Ranks = new List<Element>
+               {
+                        new Element{Value= "Shubham", IsKnownValue =true},
+                        new Element{Value= "Chris", IsKnownValue =false},
+                        new Element{Value= "Joey", IsKnownValue =false},
+                        new Element{Value= "Miranda", IsKnownValue =false},
+                        new Element{Value= "Sammie", IsKnownValue =false},
+                        new Element{Value= "Mercedeze", IsKnownValue =false},
+               }
+           }
+           );
+
+
+            var rankSolver = new RankSolver(solution, rating);
+
+
+            var result = rankSolver.Solve();
+
+            Console.WriteLine("Possible configurations for solution");
+            int count = 1;
+            foreach (var item in solution)
+            {
+                Console.WriteLine($"{count} - {item}");
+                count++;
+            }
+
+            foreach (var res in result)
+            {
+                Console.WriteLine("Solution 1");
+                foreach (var item in res.Rankings)
+                {
+                    Console.WriteLine($"Ranks for {item.Id}");
+                    int place = 0;
+                    foreach (var rank in item.Ranks)
+                    {
+                        place++;
+                        Console.WriteLine($"{place} - {rank.Value}");
+                    }
+                }
+            }
+
+
+        }
+
+        private static void GenerateRakings()
+        {
+            var players = new List<string>
+            {
+                "Alana", "Antonio", "Mercedeze", "Rebecca", "Chris", "Sammie", "Shubham", "Joey"
+            };
+
+            var rankDictionary = new Dictionary<string, int>();
+
+            foreach (var item in players)
+            {
+                rankDictionary.Add(item, 0);
+            }
+
+            var votes = new List<List<string>>();
+
+            for (int i = 0; i < 8; i++)
+            {
+                var vote = players.Shuffle();
+                votes.Add(vote);
+            }
+
+            //calculate votes
+            foreach (var vote in votes)
+            {
+                foreach (var name in vote)
+                {
+                    rankDictionary[name] += vote.IndexOf(name);
+                }
+            }
+
+            //display votes
+
+            int place = 0;
+            foreach (KeyValuePair<string, int> item in rankDictionary.OrderBy(key => key.Value))
+            {
+                place++;
+                Console.WriteLine($"{place} - {item.Key}");
+            }
+        }
+    }
+
+    public static class Helper
+    {
+        private static Random rng = new Random();
+
+        public static List<T> Shuffle<T>(this List<T> list)
+        {
+            int n = list.Count;
+            while (n > 1)
+            {
+                n--;
+                int k = rng.Next(n + 1);
+                T value = list[k];
+                list[k] = list[n];
+                list[n] = value;
+            }
+
+            return list;
+        }
+
+    }
+
+}
---
+++ RankVotingSolver/RankVotingSolver/RankSolver.cs
@@ -0,0 +1,213 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RankVotingSolver
+{
+    public class RankSolver
+    {
+        private readonly List<string> Solution;
+        private Ratings Ratings;
+        private Dictionary<string, int> RatingsCode = new Dictionary<string, int>();
+        private Queue<Ratings> RatingsQueue = new Queue<Ratings>();
+
+        public RankSolver(List<string> solution, Ratings initialRatings)
+        {
+            Solution = solution;
+            Ratings = initialRatings;
+        }
+
+        public List<Ratings> Solve()
+        {
+            var result = new List<Ratings>();
+
+            RatingsQueue.Enqueue(Ratings);
+
+            while (RatingsQueue.Any())
+            {
+                Debug.WriteLine($"Items in RatingsQueue: {RatingsQueue.Count}");
+                Debug.WriteLine($"Items in Ratings Code: {RatingsCode.Count}");
+                Debug.WriteLine($"Results: {result.Count}");
+
+                var currentRating = RatingsQueue.Dequeue();
+
+                if (RatingsCode.ContainsKey(currentRating.GetCode()))
+                    continue;
+
+                if (IsSolved(currentRating))
+                {
+                    result.Add(currentRating);
+                    RatingsCode.ContainsKey(currentRating.GetCode());
+                    continue;
+                }
+
+                foreach (var r in currentRating.GetChildren())
+                {
+                    if (RatingsCode.ContainsKey(r.GetCode()))
+                        continue;
+
+                    RatingsQueue.Enqueue(r);
+                }
+
+                RatingsCode.Add(currentRating.GetCode(), 0);
+            }
+
+            return result;
+        }
+
+        private bool IsSolved(Ratings ratings)
+        {
+            return Solution.SequenceEqual(ratings.FinalRanking);
+        }
+    }
+
+
+    public class Element
+    {
+        public string Value { get; set; }
+        public bool IsKnownValue { get; set; }
+    }
+
+    public class Ranking
+    {
+        public string Id { get; set; }
+
+
+        public Ranking()
+        {
+            Id = Guid.NewGuid().ToString();
+        }
+
+        public Ranking(string name)
+        {
+            Id = name;
+        }
+
+
+        public List<Element> Ranks { get; set; } = new List<Element>();
+
+        public List<Ranking> GetChildren()
+        {
+            var result = new List<Ranking>();
+
+            for (int startIndex = 0; startIndex < Ranks.Count - 1; startIndex++)
+            {
+                for (int swapIndex = startIndex + 1; swapIndex < Ranks.Count; swapIndex++)
+                {
+                    var newRanking = new Ranking();
+                    newRanking.Id = Id;
+
+                    newRanking.Ranks.AddRange(Ranks);
+
+                    newRanking.Swap(startIndex, swapIndex);
+
+                    result.Add(newRanking);
+                }
+
+            }
+
+            return result;
+        }
+
+        private void Swap(int startIndex, int swapIndex)
+        {
+            if (Ranks[startIndex].IsKnownValue)
+                return;
+            if (Ranks[swapIndex].IsKnownValue)
+                return;
+
+            var temp = Ranks[startIndex];
+            Ranks[startIndex] = Ranks[swapIndex];
+            Ranks[swapIndex] = temp;
+        }
+
+    }
+
+    public class Ratings
+    {
+        public List<Ranking> Rankings { get; set; } = new List<Ranking>();
+        private List<string> finalRanking = new();
+
+        public List<string> FinalRanking
+        {
+            get
+            {
+                if (finalRanking.Any())
+                    return finalRanking;
+
+                finalRanking = CalculateFinalRanking();
+
+                return finalRanking;
+            }
+
+        }
+
+        private List<string> CalculateFinalRanking()
+        {
+            var rankDictionary = new Dictionary<string, int>();
+
+            foreach (var item in Rankings.First().Ranks)
+            {
+                rankDictionary.Add(item.Value, 0);
+            }
+
+            foreach (var item in Rankings.Skip(1).First().Ranks)
+            {
+                if (!rankDictionary.ContainsKey(item.Value))
+                    rankDictionary.Add(item.Value, 0);
+            }
+
+            foreach (var rank in Rankings)
+            {
+                foreach (var name in rank.Ranks)
+                {
+                    if (rankDictionary.ContainsKey(name.Value))
+                        rankDictionary[name.Value] += rank.Ranks.IndexOf(name);
+                }
+            }
+
+            return rankDictionary.OrderBy(key => key.Value).Select(x => x.Key).ToList();
+        }
+
+        public List<Ratings> GetChildren()
+        {
+            var result = new List<Ratings>();
+
+            for (int index = 0; index < Rankings.Count; index++)
+            {
+                var rankChildren = Rankings[index].GetChildren();
+
+                foreach (var item in rankChildren)
+                {
+                    var newRatings = new Ratings();
+                    newRatings.Rankings.Add(item);
+                    newRatings.Rankings.AddRange(Rankings.Where(x => x.Id != Rankings[index].Id));
+
+                    result.Add(newRatings);
+                }
+            }
+
+            return result;
+        }
+
+        public string GetCode()
+        {
+            var result = new StringBuilder();
+
+            foreach (var rank in Rankings.OrderBy(x => x.Id))
+            {
+                foreach (var item in rank.Ranks)
+                {
+                    result.Append(item.Value);
+                }
+
+            }
+
+            return result.ToString();
+        }
+    }
+
+}
